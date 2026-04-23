6b6650327e87ddf2c49350886c66702b6d9d7fe1
AnaDariva <anadarivaramos@gmail.com>
2026-04-23 15:43:22 -0300

    86e11rv20 - Exercicio 1 - criei um programa que permite o usuario gerenciar uma lista de tarefas.

--- Exercicio_1/src/main/java/br/com/roinfo/analuisa/Main.java
+++ Exercicio_1/src/main/java/br/com/roinfo/analuisa/Main.java
@@ -1,17 +1,82 @@
 package br.com.roinfo.analuisa;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> tarefas = new ArrayList<>();
+        int opcao = 0;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        while (opcao != 4) {
+            System.out.println("---MENU DE TAREFAS---");
+            System.out.println("1 - Adicionar tarefa");
+            System.out.println("2 - Remover tarefa");
+            System.out.println("3 - Exibir tarefas");
+            System.out.println("4 - sair");
+            System.out.println("Escolha uma opção: ");
+
+            opcao = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (opcao) {
+                case 1:
+                    System.out.println("Digite o nome do tarefa: ");
+                    String tarefa = scanner.nextLine();
+                    tarefas.add(tarefa);
+                    System.out.println("Tarefa adicionada com sucesso!!");
+                    break;
+
+                case 2:
+                    if (tarefas.isEmpty()) {
+                        System.out.println("Não há tarefas para remover");
+                    } else {
+                        System.out.println("---tarefas---");
+                        for (int i = 0; i < tarefas.size(); i++) {
+                            System.out.println((i + 1) + " - " + tarefas.get(i));
+                        }
+
+                        System.out.println("digite o numero da tarefa que precisa remover: ");
+                        int indice = scanner.nextInt();
+                        scanner.nextLine();
+
+                        if (indice >= 1 && indice <= tarefas.size()) {
+                            String removida = tarefas.remove(indice - 1);
+                            System.out.println("Tarefa removida: " + removida);
+
+                        } else {
+                            System.out.println("Número inválido");
+                        }
+
+                    }
+                    break;
+
+                case 3:
+                    if (tarefas.isEmpty()) {
+                        System.out.println("Nenhuma tarefa cadastrada");
+                    } else {
+                        System.out.println("---Lista de tarefas--");
+                        for (int i = 0; i < tarefas.size(); i++) {
+                            System.out.println((i + 1) + " - " + tarefas.get(i));
+                        }
+                    }
+                    break;
+
+                case 4:
+                    System.out.println("saindo..");
+
+                    break;
+
+                default:
+                    System.out.println("opção inválida");
+
+            }
         }
+
+        scanner.close();
     }
 }
+
+
+
