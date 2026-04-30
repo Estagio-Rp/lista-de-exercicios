0ffb04eb15f63fd9faf8aab9bbf20897a0b133e3
AnaDariva <anadarivaramos@gmail.com>
2026-04-30 10:37:46 -0300

    86e12af8m - Exercicio 3 - correção de commit

--- Exercicio_3/src/main/java/orientacaoObjetos/heranca/Gerente.java
+++ Exercicio_3/src/main/java/orientacaoObjetos/heranca/Gerente.java
@@ -4,12 +4,16 @@ public class Gerente extends Funcionario {
 
 
     public Gerente(String nome, double salarioBase) {
+
         super(nome, salarioBase);
+
     }
 
     @Override
     public double calcularSalario() {
+
         return salarioBase + 2000;
+
     }
 
 }
