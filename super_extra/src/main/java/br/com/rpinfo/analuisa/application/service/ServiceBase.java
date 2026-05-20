b27752c69cacd149ab0973d8471cef7446ff76fd
AnaDariva <anadarivaramos@gmail.com>
2026-05-20 14:47:40 -0300

    86e1ejcn2 - Comentando o ServiceBase que não esta sendo usado.

--- super_extra/src/main/java/br/com/rpinfo/analuisa/application/service/ServiceBase.java
+++ super_extra/src/main/java/br/com/rpinfo/analuisa/application/service/ServiceBase.java
@@ -1,39 +1,39 @@
-package br.com.rpinfo.analuisa.application.service;
-
-import br.com.rpinfo.analuisa.Conexao;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
-public class ServiceBase {
-
-    private Connection connection;
-
-    public ServiceBase(Connection connection) {
-        this.connection = connection;
-    }
-
-    public Connection getConnection() {
-        try {
-            if (connection == null || connection.isClosed()) {
-                this.connection = connectionManager();
-            }
-
-            return connection;
-
-        } catch (SQLException e) {
-            System.out.println("Erro ao revalidar/reabrir a conexão: " + e.getMessage());
-            throw new RuntimeException("Falha ao garantir uma conexão válida.", e);
-        }
-    }
-
-    public static Connection connectionManager() {
-        try {
-            return Conexao.conectar();
-
-        } catch (Exception e) {
-            System.out.println("Erro ao abrir conexão com o banco: " + e.getMessage());
-            throw new RuntimeException("Falha ao conectar com o banco de dados.", e);
-        }
-    }
-}
+//package br.com.rpinfo.analuisa.application.service;
+//
+//import br.com.rpinfo.analuisa.Conexao;
+//
+//import java.sql.Connection;
+//import java.sql.SQLException;
+//
+//public class ServiceBase {
+//
+//    private Connection connection;
+//
+//    public ServiceBase(Connection connection) {
+//        this.connection = connection;
+//    }
+//
+//    public Connection getConnection() {
+//        try {
+//            if (connection == null || connection.isClosed()) {
+//                this.connection = connectionManager();
+//            }
+//
+//            return connection;
+//
+//        } catch (SQLException e) {
+//            System.out.println("Erro ao revalidar/reabrir a conexão: " + e.getMessage());
+//            throw new RuntimeException("Falha ao garantir uma conexão válida.", e);
+//        }
+//    }
+//
+//    public static Connection connectionManager() {
+//        try {
+//            return Conexao.conectar();
+//
+//        } catch (Exception e) {
+//            System.out.println("Erro ao abrir conexão com o banco: " + e.getMessage());
+//            throw new RuntimeException("Falha ao conectar com o banco de dados.", e);
+//        }
+//    }
+//}
