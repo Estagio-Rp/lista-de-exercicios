8b16de1c4a833a29fcba3ba3b6c0ed24ae1b5dc8
AnaDariva <anadarivaramos@gmail.com>
2026-05-28 11:48:22 -0300

    Arruma formatação de data e hora em clientes

--- super_extra/src/main/java/br/com/rpinfo/analuisa/application/dto/clientes/ClientesDTO.java
+++ super_extra/src/main/java/br/com/rpinfo/analuisa/application/dto/clientes/ClientesDTO.java
@@ -1,7 +1,7 @@
 package br.com.rpinfo.analuisa.application.dto.clientes;
 
-
 import br.com.rpinfo.analuisa.domain.model.entity.Cliente;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,27 +11,33 @@ import java.time.LocalDateTime;
 
 @Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 public class ClientesDTO {
+
     private Integer id;
     private String nome;
     private String email;
     private String cpf;
     private String telefone;
+
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
     private LocalDateTime dataCadastro;
+
     private Integer enderecoId;
 
     public Cliente toEntity() {
-        return new Cliente(
-                this.id,
-                this.nome,
-                this.email,
-                this.cpf,
-                this.telefone,
-                this.dataCadastro,
-                this.enderecoId
-        );
+        Cliente cliente = new Cliente();
+
+        cliente.setId(this.id);
+        cliente.setNome(this.nome);
+        cliente.setEmail(this.email);
+        cliente.setCpf(this.cpf);
+        cliente.setTelefone(this.telefone);
+        cliente.setDataCadastro(this.dataCadastro);
+        cliente.setEnderecoId(this.enderecoId);
+
+        return cliente;
     }
 
     public static ClientesDTO fromEntity(Cliente cliente) {
