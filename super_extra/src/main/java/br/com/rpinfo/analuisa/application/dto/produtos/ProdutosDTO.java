0b85167854a6d0532568956f4a395e5df10e356b
AnaDariva <anadarivaramos@gmail.com>
2026-06-03 10:44:48 -0300

    adiciona construtores e acessores em produto

--- super_extra/src/main/java/br/com/rpinfo/analuisa/application/dto/produtos/ProdutosDTO.java
+++ super_extra/src/main/java/br/com/rpinfo/analuisa/application/dto/produtos/ProdutosDTO.java
@@ -8,6 +8,7 @@ import lombok.Setter;
 
 import java.math.BigDecimal;
 
+
 @Getter
 @Setter
 @NoArgsConstructor
--- super_extra/src/main/java/br/com/rpinfo/analuisa/domain/model/entity/Produto.java
+++ super_extra/src/main/java/br/com/rpinfo/analuisa/domain/model/entity/Produto.java
@@ -1,17 +1,9 @@
 package br.com.rpinfo.analuisa.domain.model.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.math.BigDecimal;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 @Entity
 @Table(name = "produtos")
 public class Produto {
@@ -31,4 +23,55 @@ public class Produto {
 
     @Column(nullable = false)
     private Integer estoque;
+
+    public Produto() {
+    }
+
+    public Produto(Integer id, String nome, BigDecimal preco, String categoria, Integer estoque) {
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+        this.categoria = categoria;
+        this.estoque = estoque;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public Integer getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(Integer estoque) {
+        this.estoque = estoque;
+    }
 }
