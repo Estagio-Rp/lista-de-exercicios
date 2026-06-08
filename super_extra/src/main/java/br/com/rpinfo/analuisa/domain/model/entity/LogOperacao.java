7b857fdbd36b7f985dad9da8adf96b91a23a449f
AnaDariva <anadarivaramos@gmail.com>
2026-06-08 09:26:56 -0300

    86e1kegyr - padroniza campos de logoperacoes

--- super_extra/src/main/java/br/com/rpinfo/analuisa/domain/model/entity/LogOperacao.java
+++ super_extra/src/main/java/br/com/rpinfo/analuisa/domain/model/entity/LogOperacao.java
@@ -18,21 +18,21 @@ public class LogOperacao {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "log_id")
+    @Column(name = "logo_id")
     private Integer id;
 
-    @Column(name = "log_codigo", nullable = false)
+    @Column(name = "logo_codigo", nullable = false)
     private Integer codigoLog;
 
-    @Column(name = "log_data", nullable = false)
+    @Column(name = "logo_data", nullable = false)
     private LocalDate data;
 
-    @Column(name = "log_hora", nullable = false, length = 8)
+    @Column(name = "logo_hora", nullable = false, length = 8)
     private String hora;
 
-    @Column(name = "log_descricao", nullable = false, length = 255)
+    @Column(name = "logo_descricao", nullable = false, length = 255)
     private String descricao;
 
-    @Column(name = "log_agrupamento", nullable = false, length = 255)
+    @Column(name = "logo_agrupamento", nullable = false, length = 255)
     private String agrupamento;
 }
